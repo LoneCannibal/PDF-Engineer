ac4725f81083767a49161f312fe1156812b5b7c1
Aakash SD <69956836+LoneCannibal@users.noreply.github.com>
2023-10-09 17:32:42 +0530

    Update pdfEngineer.java

--- src/main/java/com/team25/hackathon/pdfEngineer.java
+++ src/main/java/com/team25/hackathon/pdfEngineer.java
@@ -2,11 +2,14 @@ package com.team25.hackathon;
 
 import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.InputStreamReader;
 import java.net.URL;
 
-import org.apache.pdfbox.Loader;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.preflight.Format;
 import org.apache.pdfbox.preflight.PreflightDocument;
@@ -15,10 +18,12 @@ import org.apache.pdfbox.preflight.exception.ValidationException;
 import org.apache.pdfbox.preflight.parser.PreflightParser;
 import org.apache.pdfbox.text.PDFTextStripper;
 
+import com.spire.pdf.conversion.PdfStandardsConverter;
+
 public class pdfEngineer {
 
-	PDDocument pdfDoc1;
-	File pdfFile;
+	private PDDocument pdfDoc1;
+	private File pdfFile;
 
 	// Default constructor
 	public pdfEngineer() {
@@ -31,7 +36,7 @@ public class pdfEngineer {
 		this.pdfFile = pdfFile;
 		try {
 			// Load PDF file
-			pdfDoc1 = Loader.loadPDF(this.pdfFile);
+			pdfDoc1 = PDDocument.load(this.pdfFile);
 		} catch (Exception e) {
 			System.out.println("ERROR: File could not be loaded " + e);
 		}
@@ -43,9 +48,8 @@ public class pdfEngineer {
 
 		try {
 			InputStream is = pdfURL.openStream();
-			BufferedInputStream bis = new BufferedInputStream(is);
-			byte[] temp = bis.readAllBytes();
-			pdfDoc1 = Loader.loadPDF(temp);
+			byte[] temp = is.readAllBytes();
+			pdfDoc1 = PDDocument.load(temp);
 		} catch (Exception e) {
 			System.out.println("ERROR: File could not be loaded from URL " + e);
 			System.out.println("Please recheck the given URL: " + pdfURL);
@@ -55,9 +59,10 @@ public class pdfEngineer {
 
 	// Return all the text from PDF as String
 	public String extractText() {
-		PDFTextStripper stripper = new PDFTextStripper();
 		String text = "";
+
 		try {
+			PDFTextStripper stripper = new PDFTextStripper();
 			text = stripper.getText(pdfDoc1);
 		} catch (Exception e) {
 			System.out.println("ERROR: Text could not be extracted from file " + e);
@@ -65,16 +70,39 @@ public class pdfEngineer {
 		return text;
 	}
 
+	// Compare PDF files with each other based only on text
+	// Both files need to have same text to be equal
+	public boolean simpleCompare(pdfEngineer pdf2) {
+		if (extractText().equals(pdf2.extractText()))
+			return true;
+		return false;
+
+	}
+
 	// Validate PDF according to provided format
-	public void validate(Format documentFormat) throws IOException {
+	public void validatePDF(Format documentFormat) throws Exception {
 		ValidationResult result = null;
 		PreflightParser parser = new PreflightParser(pdfFile);
 		parser.parse(documentFormat);
-		PreflightDocument document = parser.getPDDocument();
-		document.validate();
-		result = document.getResult();
-		System.out.println(result);
+		PreflightDocument documentTemp = parser.getPreflightDocument();
+		documentTemp.validate();
+		documentTemp.close();
+		result = documentTemp.getResult();
 		
+		if(result.getErrorsList().size() > 0) {
+			for(ValidationResult.ValidationError ve: result.getErrorsList()) {
+				System.out.println(ve.getErrorCode()+": "+ve.getDetails()+": "+ve.getPageNumber());
+				//System.out.println(ve.toString());
+			}
+		}
+		System.out.println(result.toString());
 
+	}
+	
+	//Convert PDF to given Format
+	public void convertToFormat(Format documentFormat, String outputFile) throws Exception {
+		FileInputStream isr = new FileInputStream(pdfFile);
+		PdfStandardsConverter converter = new PdfStandardsConverter(isr);
+		converter.toPdfA1A(outputFile);
 	}
 }
